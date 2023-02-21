16abede19c15ebffeb8516d962338ec8423ea780
asrar bhat <asrarpsi@gmail.com>
2023-02-21 22:43:19 +0530

    yellow

---
+++ 103_earn/105_code/selenus/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.setProperty("webdriver.chrome.driver","/home/rayon/Downloads/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+//        driver.navigate().to("www.github.com");
+    }
+}
